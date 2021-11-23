f42ca8a2486c28bea5063782e2bce037a5911f10
Lukáš Paukert <paukeluk@fit.cvut.cz>
2021-11-23 17:03:12 +0100

    Add attribute parent to Team entity

--- backend/src/Entity/Team.php
+++ backend/src/Entity/Team.php
@@ -34,10 +34,21 @@ class Team
 	 */
 	private $teamRoles;
 
+	/**
+	 * @ORM\ManyToOne(targetEntity=Team::class, inversedBy="children")
+	 */
+	private $parent;
+
+	/**
+	 * @ORM\OneToMany(targetEntity=Team::class, mappedBy="parent")
+	 */
+	private $children;
+
 	public function __construct()
 	{
 		$this->rooms = new ArrayCollection();
 		$this->teamRoles = new ArrayCollection();
+		$this->children = new ArrayCollection();
 	}
 
 	public function getId(): ?int
@@ -111,6 +122,48 @@ class Team
 			// set the owning side to null (unless already changed)
 			if ($teamRole->getTeam() === $this) {
 				$teamRole->setTeam(null);
+			}
+		}
+
+		return $this;
+	}
+
+	public function getParent(): ?self
+	{
+		return $this->parent;
+	}
+
+	public function setParent(?self $parent): self
+	{
+		$this->parent = $parent;
+
+		return $this;
+	}
+
+	/**
+	 * @return Collection|self[]
+	 */
+	public function getChildren(): Collection
+	{
+		return $this->children;
+	}
+
+	public function addChild(self $child): self
+	{
+		if (!$this->children->contains($child)) {
+			$this->children[] = $child;
+			$child->setParent($this);
+		}
+
+		return $this;
+	}
+
+	public function removeChild(self $child): self
+	{
+		if ($this->children->removeElement($child)) {
+			// set the owning side to null (unless already changed)
+			if ($child->getParent() === $this) {
+				$child->setParent(null);
 			}
 		}
 
