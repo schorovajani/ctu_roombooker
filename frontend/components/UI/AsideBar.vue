4dba9bdae5dbf3f4b3f8c78996aaf4f0dd386358
Jana Schořová <schorjan@fit.cvut.cz>
2021-12-07 10:30:12 +0100

    Reside aside bar

--- frontend/components/UI/AsideBar.vue
+++ frontend/components/UI/AsideBar.vue
@@ -18,11 +18,11 @@ export default {}
 
 <style scoped>
 .aside-bar {
-  height: 903px;
-  width: 515px;
-  background-image: url('~/assets/UI/aside.png');
+  height: 789px;
+  width: 450px;
+  background-image: url('~/assets/UI/aside_v1.png');
   margin: 3rem 0 2rem 0;
-  padding: 5rem 10rem 7rem 2rem;
+  padding: 5rem 8.5rem 7rem 1.5rem;
   position: relative;
   display: flex;
   flex-direction: column;
@@ -43,6 +43,7 @@ export default {}
 }
 
 .aside-text {
+  text-align: center;
   display: flex;
   flex-direction: column;
   align-items: center;
