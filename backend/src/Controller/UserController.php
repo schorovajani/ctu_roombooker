216cf2f97ab548fb68daa5bf8dc4aeb023bda444
Michal Dvořák <dvora124@fit.cvut.cz>
2021-12-13 17:44:56 +0100

    Use @Rest\XXX() over Symfony's @Route for REST controller

--- backend/src/Controller/UserController.php
+++ backend/src/Controller/UserController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Service\UserService;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -23,7 +24,7 @@ class UserController extends AbstractFOSRestController
   }
 
   /**
-   * @Route("/users", methods={"GET"})
+   * @Rest\Get("/users")
    * @return Response
    */
   public function getAllUsers () : Response
