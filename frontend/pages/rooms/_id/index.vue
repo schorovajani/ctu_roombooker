534d4f13dea6931460b28762f4ce6e385cfdc400
Jana Schořová <schorjan@fit.cvut.cz>
2021-12-28 14:15:18 +0100

    Add calendar

---
+++ frontend/pages/rooms/_id/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <section>
+    <div>
+      <h2>{{ building }}:{{ room }}</h2>
+      <div>
+        <button>left</button>
+        <span>{{ month }}</span>
+        <button>right</button>
+      </div>
+    </div>
+    <v-calendar
+      ref="calendar"
+      type="week"
+      :now="today"
+      :weekdays="weekdays"
+      :events="events"
+    ></v-calendar>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      building: 'T9',
+      room: '343',
+      now: new Date('2021-12-15'),
+      weekdays: [1, 2, 3, 4, 5, 6, 0],
+      color: 'red',
+      events: [
+        {
+          name: 'title1',
+          start: '2021-12-22 09:00',
+          end: '2021-12-22 11:00',
+        },
+        {
+          name: 'title2',
+          start: '2021-12-21 14:00',
+          end: '2021-12-21 15:00',
+        },
+      ],
+    }
+  },
+  computed: {
+    today() {
+      return this.now.toISOString().substring(0, 10)
+    },
+    month() {
+      return this.now.getMonth()
+    },
+  },
+}
+</script>
+
+<style scoped>
+section {
+  margin: 10rem auto;
+  width: 90%;
+}
+</style>
---
+++ frontend/pages/rooms/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <RoomsCard />
+</template>
+
+<script>
+import RoomsCard from '~/components/rooms/RoomsCard.vue'
+export default {
+  components: { RoomsCard },
+}
+</script>
