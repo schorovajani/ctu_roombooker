d47143c259b60efe9e7f18c7e2a94f3a9ed7f0de
Michal Dvořák <dvora124@fit.cvut.cz>
2021-12-29 15:29:01 +0100

    Filter duplicates when returning /api/rooms/{id}/users

--- backend/src/Controller/RoomController.php
+++ backend/src/Controller/RoomController.php
@@ -52,6 +52,13 @@ class  RoomController extends \FOS\RestBundle\Controller\AbstractFOSRestControll
 				}
 
 				$users = array_merge($users1, $users2);
+				$users = array_unique($users, SORT_REGULAR);
+				// array_unique retains keys even when the origin array didn't have them, e.g. [1, 1, 2] becomes ["0" => 1, "2" => 2].
+			  // Possible explanation: https://www.php.net/manual/en/function.array-unique.php
+				// > [...] If multiple elements compare equal under the given flags, then the key and value of the first equal
+				// > element will be retained.
+			  // It is possible that other places where array_unique is used are affected
+				$users = array_values($users);
 				return $this->handleView($this->view($users, Response::HTTP_OK));
 
 			default:
