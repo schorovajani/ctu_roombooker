5d8c3643e41c834b910c231d2edd900fa04a3fde
Lukáš Paukert <paukeluk@fit.cvut.cz>
2022-01-01 15:27:53 +0100

    Add authorization for GET requests for /users endpoints

--- backend/src/Controller/UserController.php
+++ backend/src/Controller/UserController.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use App\Service\UserService;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -26,33 +27,44 @@ class UserController extends AbstractFOSRestController
 
 	/**
 	 * @Rest\Get("/users")
+	 * @IsGranted("IS_AUTHENTICATED_REMEMBERED")
+	 *
 	 * @return Response
 	 */
 	public function routeGetUsers(): Response
 	{
-		$users = $this->userService->getAll();
-		$view = $this->view($users, Response::HTTP_OK);
+		$view = $this->view($this->userService->getAll(), Response::HTTP_OK);
+		$view->getContext()->setGroups(['listUser']);
 		return $this->handleView($view);
 	}
 
 	/**
 	 * @Rest\Get("/users/{id}", requirements={"id": "\d+"})
+	 * @IsGranted("IS_AUTHENTICATED_REMEMBERED")
+	 *
 	 * @param User $user
 	 * @return Response
 	 */
 	public function routeGetUser(User $user): Response
 	{
-		return $this->handleView($this->view($user, Response::HTTP_OK));
+		$view = $this->view($user, Response::HTTP_OK);
+		$view->getContext()->setGroups(['listUser']);
+		return $this->handleView($view);
 	}
 
 	/**
 	 * @Rest\Get("/users/{id}/{attr}", requirements={"id": "\d+"})
+	 * @IsGranted("IS_AUTHENTICATED_REMEMBERED")
+	 *
 	 * @param User $user
 	 * @param string $attr
 	 * @return Response
 	 */
 	public function routeGetUserAttr(User $user, string $attr): Response
 	{
+		if (!$this->isGranted('ROLE_ADMIN') && $this->getUser()->getOwner() !== $user)
+			throw $this->createAccessDeniedException();
+
 		switch ($attr) {
 			case "requests":
 				$viewData = $this->userService->getUserRequests($user);
@@ -70,6 +82,9 @@ class UserController extends AbstractFOSRestController
 			default:
 				throw $this->createNotFoundException();
 		}
-		return $this->handleView($this->view($viewData, Response::HTTP_OK));
+
+		$view = $this->view($viewData, Response::HTTP_OK);
+		$view->getContext()->setGroups(['listBuilding', 'listRequest', 'listRoom', 'listStatus', 'listTeam', 'listUser']);
+		return $this->handleView($view);
 	}
 }
